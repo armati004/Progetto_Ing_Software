eb943883bc2abf83811d742c1bd64b52aac4c9cb
lcacciarru <l.cacciarru@studenti.unibg.it>
2025-12-05 14:47:00 +0100

    Modifica alla classe Oggetto

--- HogwartsBattle/HogwartsBattle/src/main/java/carte/Oggetto.java
+++ HogwartsBattle/HogwartsBattle/src/main/java/carte/Oggetto.java
@@ -22,28 +22,6 @@ public class Oggetto extends Carta {
         super(nome, id, classe, descrizione, costo, pathImmagine, effetti, triggers);
     }
 
-    @Override
-    public void applicaEffetto(StatoDiGioco stato, Giocatore attivo) {
-
-        for (Effetto e : getEffetti()) {
-
-            if (e.getType() == TipoEffetto.SCELTA || e.getTarget() == BersaglioEffetto.SCELTA_MULTIPLA) { // da inserire il file con le scelte multiple  
-                gestisciScelta(e, stato, attivo);
-            } else {
-                EsecutoreEffetti.eseguiEffetto(e, stato, attivo);
-            }
-        }
-    }
-    private void gestisciScelta(Effetto effetto, StatoDiGioco stato, Giocatore attivo) {
-        // Logica per gestire l'effetto di scelta
-        // Ad esempio, presentare le opzioni al giocatore e applicare l'effetto scelto
-        List<Effetto> opzioni = effetto.getOpzioni();
-        if (opzioni != null && !opzioni.isEmpty()) {
-            // Per semplicità, scegliamo sempre la prima opzione in questo esempio
-            Effetto scelta = opzioni.get(0);
-            EsecutoreEffetti.eseguiEffetto(scelta, stato, attivo);
-        }
-    }
 
 }
 
