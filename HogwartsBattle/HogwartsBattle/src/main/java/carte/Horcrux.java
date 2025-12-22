8a4452ba79f64ac4be558cc2eabda3ae84b954e0
RobyPana99 <a.panaite@studenti.unibg.it>
2025-12-22 10:07:09 +0100

    implementazione

--- HogwartsBattle/HogwartsBattle/src/main/java/carte/Horcrux.java
+++ HogwartsBattle/HogwartsBattle/src/main/java/carte/Horcrux.java
@@ -1,10 +1,7 @@
 package carte;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import com.google.gson.annotations.SerializedName;
 
 import gestoreEffetti.Effetto;
 import gestoreEffetti.Trigger;
@@ -12,19 +9,21 @@ import grafica.Entita;
 
 public class Horcrux extends Carta {
 	
-	@SerializedName("destroyCondition")
 	private List<Entita> segnaliniRichiesti;
 	
 	private Set<Entita> segnaliniAssegnati;
 	
 	public Horcrux(String nome, String id, String classe, String descrizione, int costo, String pathImmagine,
-			List<Effetto> effetti, List<Trigger> triggers, List<Entita> segnaliniRichiesti) {
+			List<Effetto> effetti, List<Trigger> triggers) {
 		super(nome, id, classe, descrizione, costo, pathImmagine, effetti, triggers);
-		this.segnaliniRichiesti = segnaliniRichiesti;
-		this.segnaliniAssegnati = new HashSet<>();
+		
 	}
 	
 	public Boolean applicaRisultatoDado(Entita facciaDado) {
+		if (segnaliniRichiesti.contains(facciaDado) && !segnaliniAssegnati.contains(facciaDado)) {
+			segnaliniAssegnati.add(facciaDado);
+			return true;
+		}
 		return false;
 	}
 	
